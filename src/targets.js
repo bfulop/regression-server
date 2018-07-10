234d00414ff2570dacb6be4d09883996064680c5
Balint Fulop <bfulop@mac.com>
2018-07-10 13:22:38 +0200

    Simplify adding targets for all screen sizes

--- src/targets.js
+++ src/targets.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const R = require('ramda')
 
 const paths = {
   test: path.resolve('./captures/testDir'),
@@ -7,20 +8,54 @@ const paths = {
 
 const baseUrl = 'http://127.0.0.1:3014/app_dev.php/'
 
-const testtargets = [
+let testtargets = [
   {
-    route: 'all/film/balint-test',
+    route: 'film/balint-test',
+    elements: ['header.pageheader', '.filmsummary', '.filmawards .sectionheader', '.filmlabels__item', '.filmcredits .simpleblockheader', '.filmpress__header', '.filmdirector__header', '.filmdirector__actions', '.filmdirector__filmslistheader', '.filmslist__afilm'],
     targets: [
       {
         width: 1200,
-        elements: ['header.pageheader']
+        elements: []
       },
       {
         width: 768,
-        elements: ['header.pageheader']
+        elements: []
+      },
+      {
+        width: 383,
+        elements: []
       }
     ]
-  }
+  },
+  // {
+  //   route: null,
+  //   elements: ['li.filmcard', '.homehero'],
+  //   targets: [
+  //     {
+  //       width: 1200,
+  //       elements: []
+  //     },
+  //     {
+  //       width: 768,
+  //       elements: []
+  //     },
+  //     {
+  //       width: 383,
+  //       elements: []
+  //     }
+  //   ]
+  // }
 ]
+
+function mergeTargetElements (anelem) {
+  const elems = R.prop('elements', anelem)
+  function addParentElements(item) {
+    return R.merge(item, {elements: R.concat(item.elements, elems)})
+  }
+  return R.merge(anelem, {targets: R.compose(R.map(addParentElements), R.prop('targets'))(anelem)})
+}
+
+testtargets = R.map(mergeTargetElements)(testtargets)
+
 
 module.exports = { testtargets, baseUrl, paths }
